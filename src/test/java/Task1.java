3630734a87bee2a820c18eeebdc0dc6df4b15b79
Ana Khurtsidze <anakhurtsidze@credo.ge>
2024-10-24 20:44:42 +0400

    Task1-Khurtsidze

---
+++ src/test/java/Task1.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Task1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Please enter number: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        System.out.println("Is " + num + " even? " + isEven(num));
+    }
+
+
+    public  boolean isEven(int i){
+        if(i%2 == 0){
+            return true;
+        }else {
+            return  false;
+        }
+    }
+}
